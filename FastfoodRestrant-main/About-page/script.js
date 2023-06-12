1a02d0db4f81826de59379ab3c6ef6847c71a12d
manar maher <manarmaher835@gmail.com>
2023-06-12 19:32:01 +0300

    first commit

---
+++ FastfoodRestrant-main/About-page/script.js
@@ -0,0 +1,51 @@
+var ele1 = document.getElementById('details1');
+function displayDetails1(){
+    setTimeout(function(){
+        ele1.style.display = "block";  
+    },100)
+}
+function leave1(){
+    setTimeout(function(){
+    ele1.style.display='none'
+    },100)
+}
+
+
+var ele2 = document.getElementById('details2');
+function displayDetails2(){
+    setTimeout(function(){
+    ele2.style.display = "block"; 
+    },100)
+}
+function leave2(){
+    setTimeout(function(){
+    ele2.style.display='none'
+    },100)
+}
+
+
+var ele3 = document.getElementById('details3');
+function displayDetails3(){
+    setTimeout(function(){
+        ele3.style.display = "block"; 
+    },100)
+}
+function leave3(){
+    setTimeout(function(){
+    ele3.style.display='none'
+    },100)
+}
+
+
+var ele4 = document.getElementById('details4');
+function displayDetails4(){
+    setTimeout(function(){
+        ele4.style.display = "block"; 
+    },100)
+}
+function leave4(){
+    setTimeout(function(){
+    ele4.style.display='none'
+    },100)
+}
+
